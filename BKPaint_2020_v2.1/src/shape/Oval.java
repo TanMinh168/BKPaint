07a96e31d7408cb3a90a276c63fe8087295ae216
ngoviethoang <nnvvhh148@gmail.com>
2020-06-05 23:47:34 +0700

    Add comments to Oval.java - v2.1

--- BKPaint_2020_v2.1/src/shape/Oval.java
+++ BKPaint_2020_v2.1/src/shape/Oval.java
@@ -11,7 +11,11 @@ import java.util.ArrayList;
 public class Oval extends Shape implements DrawType {
 
     private Color fillColor;
-
+    /**
+     * Neu fillcolor khac mau trang thi do mau va ve vien .
+     * Neu khong thi chi ve vien.
+     * @param g2d doi tuong do hoa 2D
+     */
     public void draw(Graphics2D g2d) {
          BasicStroke stroke = new BasicStroke(strokeThickness,endStrokeCap,lineStrokeJoin,miterLimit,
         dashArray,dashPhase);
@@ -21,7 +25,12 @@ public class Oval extends Shape implements DrawType {
             g2d.fillOval(Math.min(start.x, end.x), Math.min(start.y, end.y),
                     Math.abs(start.x - end.x), Math.abs(start.y - end.y));
         }
+        // strokeColor o class Shape
         g2d.setColor(strokeColor);
+        /*
+        Diem bat dau luon la diem ben tren va ben trai ( goc phan tu thu 2 )
+        Hai tham so sau la duong kinh
+        */
         g2d.drawOval(Math.min(start.x, end.x), Math.min(start.y, end.y),
                 Math.abs(start.x - end.x), Math.abs(start.y - end.y));
     }
